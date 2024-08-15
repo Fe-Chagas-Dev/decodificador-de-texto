ca0befd7620b6fa5386eeb9d076904469bf20eb1
Fe-Chagas-Dev <fernando.chagas.dev@gmail.com>
2024-08-15 17:04:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,59 @@
-window.onload = function() {
+window.onload = function () {
     document.getElementById('text-area').focus();
 };
 
-document.querySelector('textarea').addEventListener('keypress', function(e) {
+document.querySelector('textarea').addEventListener('keypress', function (e) {
     const char = String.fromCharCode(e.keyCode || e.which);
     const pattern = /^[a-z\s]+$/;
 
     if (!pattern.test(char)) {
         e.preventDefault();
+        alert("Só é permitido letras minúsculas e sem acento.");
     }
 });
 
 function criptografar() {
     const input = document.getElementById('text-area').value;
-    let output = input.replace(/e/g, 'enter')
-                      .replace(/i/g, 'imes')
-                      .replace(/a/g, 'ai')
-                      .replace(/o/g, 'ober')
-                      .replace(/u/g, 'ufat');
-    mostrarResultado(output);
-    document.getElementById('text-area').value = "";
-    document.getElementById('text-area').focus();
+    const pattern = /^[a-z\s]+$/;
+
+    if (!pattern.test(input)) {
+        alert("Só é permitido letras minúsculas e sem acento.");
+        document.getElementById('text-area').value = "";
+        document.getElementById('text-area').focus();
+        e.preventDefault();
+
+    } else {
+        let output = input.replace(/e/g, 'enter')
+            .replace(/i/g, 'imes')
+            .replace(/a/g, 'ai')
+            .replace(/o/g, 'ober')
+            .replace(/u/g, 'ufat');
+        mostrarResultado(output);
+        document.getElementById('text-area').value = "";
+        document.getElementById('text-area').focus();
+    }
 }
 
 function descriptografar() {
     const input = document.getElementById('text-area').value;
-    let output = input.replace(/enter/g, 'e')
-                      .replace(/imes/g, 'i')
-                      .replace(/ai/g, 'a')
-                      .replace(/ober/g, 'o')
-                      .replace(/ufat/g, 'u');
-    mostrarResultado(output);
-    document.getElementById('text-area').value = "";
-    document.getElementById('text-area').focus();
+    const pattern = /^[a-z\s]+$/;
+
+    if (!pattern.test(input)) {
+        alert("Só é permitido letras minúsculas e sem acento.");
+        document.getElementById('text-area').value = "";
+        document.getElementById('text-area').focus();
+        e.preventDefault();
+
+    } else {
+        let output = input.replace(/enter/g, 'e')
+            .replace(/imes/g, 'i')
+            .replace(/ai/g, 'a')
+            .replace(/ober/g, 'o')
+            .replace(/ufat/g, 'u');
+        mostrarResultado(output);
+        document.getElementById('text-area').value = "";
+        document.getElementById('text-area').focus();
+    }
 }
 
 function mostrarResultado(texto) {
